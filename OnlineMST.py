40327d8775a287976c3cfbfe26170fa56785c85f
umarfarooq96 <umar19960607@gmail.com>
2017-02-02 01:47:58 -0500

    Update OnlineMST.py

--- OnlineMST.py
+++ OnlineMST.py
@@ -1,140 +1,75 @@
 from random import *
 from time import time
-import cProfile
+from math import sqrt
+import sys
 
 
-
-class Graph:
+class point:
     
     def __init__(self):
         
-        self.e = set()
-        self.v = set()
-        
-    def addEdge(self,V1,V2):
-        
-        if V1 not in self.v:
-            self.v = self.v | set([V1])
+        self.x = random()
+        self.y = random()
 
-        if V2 not in self.v:
-            self.v = self.v | set([V2])
-
-        for (u,v,w) in self.e:
-            if ((u == V1) and (v == V2)) or ((u == V2) and (v == V1)):
-                return
-
-        w = uniform(0,1)
-        self.e = self.e | set([(V1, V2 , w)]) 
+def dist(a,b):
     
-    def addEdges(self,V,E):
-        
-        self.v = set(V)
-        self.e = set(E)
-   
-    def getEdges(self):
-        
-        return list(self.e)
-    
-    def sortedEdges(self):
-        e = self.getEdges()
-        return sorted(e, key=getWeight)
-    
-    def __str__(self):
-        
-        return str(self.e)
-    
-    def __repr__(self):
-        
-        return str(self.sortedEdges())    
-    
-    def getWeight(e):
-        
-        return e[2]
-
-    def addNodes(self, size):
-        V = [str(i) for i in range(size)]
-        for node in V:
-            self.v.add(node)
-                 
-    def addEdgesForOneNode(self, V1):
-        for V2 in self.v:
-            if V2 != V1:
-                self.addEdge(V1, V2)
-
-    def clearCycleEdges(self, setOfEdgesTaken, setOfNodes):
-        setOfAllEdges = set()
-        for node1 in setOfNodes:
-            for node2 in setOfNodes:
-                if (node1 != node2):
-                    setOfAllEdges.add(self.getEdge(node1, node2))
-        
-        return (setOfAllEdges - setOfEdgesTaken)
-
-    def getEdge(self, V1, V2):
-        for (u,v,w) in self.e:
-            if ((u == V1) and (v == V2)) or ((u == V2) and (v == V1)):
-                return (u,v,w)
-
-    def getEdgeWeight(self, V1, V2):
-        for (u,v,w) in self.e:
-            if ((u == V1) and (v == V2)) or ((u == V2) and (v == V1)):
-                return w
-
-    def takeMinEdge(self, set1, set2): #min edge from two set of nodes
-        node1=set1.pop()
-        node2=set2.pop()
-        minEdge = self.getEdge(node1, node2)
-        set1.add(node1)
-        set2.add(node2)
-        
-        for node1 in set1:
-            for node2 in set2:
-                if self.getEdge(node1, node2) < minEdge:
-                    minEdge = self.getEdge(node1, node2)
-        return minEdge
-
-    def takeMinOutEdgeNode(self, node):
-
-        minEdge = ('0','0', 1.5)
-        for (u,v,w) in self.e:
-            if(u==node) and (w < minEdge[2]):
-                minEdge = (u,v,w)
-                
-        return minEdge
-
-    def initDict(self):
-        d = {}
-        for node in self.v:
-            d[node] = float("inf")
-        d['1'] = 0
-        return d
-        
-
-def MST(G):
-    taken = set()
-    notTaken = G.v
-    d = G.initDict()
-    
-    while len(d) != 0:
-        
+    d = sqrt((a.x-b.x)**2 + (a.y-b.y)**2)
     
     return d
 
-def getMinDict(d):
-    return min(d, key=d.get)
+def makeEdges(V, origin):
+    
+    E = []
+    for v in V:
+            if(v != origin):
+
+                E += [(origin, v, dist(origin, v))]
+    return E
+
+def MST(V):
+    
+    unvisited = [point() for i in range(V)]    
+    nv = unvisited[0]
+    unvisited = unvisited[1:]
+    edges = []
+    mst = []
+    seen = {}
+    
+    for i in unvisited: #no V has been seen 
+        seen[i] = 0
+        
+    for i in range(V-1):
+        
+        edges = sortedEdges(edges + makeEdges(unvisited, nv)) #all now visable edges
+        mst += [edges[0]] #pick smallest edge
+        
+        edges = edges[1:] #remove the edge we picked
             
+        nv = mst[-1][1] #new V that has now been seen    
+        seen[nv] = 1
+        
+        unvisited.remove(nv) #remove newly found v
+        edges = [edges for e in edges if seen[e[1]]] #remove all other edges to nv
+    
+    return mst
+    
+def getWeight(e):
+        
+    return e[2]
 
-def foo():
-    t = time()
-    G = Graph()
-    #print(time() - t)
-    G.addNodes(3)
-    print(G.v)
-    print(MST(G))
-    print(getMinDict(MST(G)))
+def sortedEdges(E):
+        return sorted(E, key=getWeight)
+    
 
-    #t = time()
-    #m = MST(G1)
-    #print(time() - t)    
+def circleMST(V):
+    m = MST(V)
+    s = 0
+    for i in m:
+        s += i[2]
+        
+    return s
+    
+    
 
-foo()
+argv = sys.argv
+print(circleMST(int(argv[1])))
